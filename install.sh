62e85b96af7e9cfa5da3939b8ef04a2ed5d3fc66
Kenneth Skovhede <kenneth@hexad.dk>
2012-07-16 21:09:15 +0200

    Added scripts to install/uninstall. Added binary for easy install.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir /opt/local/lib/columns
+cp columns.exe /opt/local/lib/columns/
+
+cp columns-launcher.sh /opt/local/bin/columns
+chmod +x /opt/local/bin/columns
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm /opt/local/lib/columns/columns.exe
+rmdir /opt/local/lib/columns
+
+rm /opt/local/bin/columns
